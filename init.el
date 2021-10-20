8ba0d6d6c24062447a964caadc667931b9e8b581
junp <jpchen78@foxmail.com>
2021-10-20 14:18:31 +0800

    use use-package to refactor

--- init.el
+++ init.el
@@ -3,12 +3,20 @@
                          ("melpa" . "http://elpa.emacs-china.org/melpa/")))
 (package-initialize)
 
+;; refresh package list if it is not already available
+(when (not package-archive-contents) (package-refresh-contents))
+
+;; install use-package if it isn't already installed
+(when (not (package-installed-p 'use-package))
+  (package-install 'use-package))
+
 (menu-bar-mode -1)
 (tool-bar-mode -1)
 (toggle-scroll-bar -1)
 (setq scroll-step 1)
 (setq scroll-conservatively  10000)
 (blink-cursor-mode 0)
+(column-number-mode 1)
 
 (setq auto-save-default nil)
 (setq make-backup-files nil)
@@ -16,57 +24,15 @@
 (setq max-lisp-eval-depth 10000)
 (setq max-specpdl-size 10000)
 
-;; (add-to-list 'default-frame-alist '(background-color . "black"))
-;; (set-face-attribute hl-line-face nil :underline t)
-;; (set-face-background 'hl-line "black")
-
 ;; save recent opened file
 (recentf-mode 1)
 (setq recentf-max-menu-items 25)
 (setq recentf-max-saved-items 25)
-(global-set-key (kbd "C-x r") 'helm-recentf)
 
-;; cl - Common Lisp Extension
-(require 'cl)
-
-;; Add Packages
-(defvar my/packages
-  '(;; Package control
-    use-package
-    ;; colorful
-    doom-themes
-    ;; Vim emu
-    evil
-    undo-fu
-    ;; Helm
-    helm
-    helm-ag
-    ;; For code
-    company
-    flycheck
-    lsp-mode
-    go-mode
-    haskell-mode
-    exec-path-from-shell
-    ))
-
-(setq package-selected-packages my/packages)
-
-(defun my/packages-installed-p ()
-  (loop for pkg in my/packages
-        when (not (package-installed-p pkg)) do (return nil)
-        finally (return t)))
-
-(unless (my/packages-installed-p)
-  (message "%s" "Refreshing package database...")
-  (package-refresh-contents)
-  (dolist (pkg my/packages)
-    (when (not (package-installed-p pkg))
-      (package-install pkg))))
-
-(require 'exec-path-from-shell)
-;; Find Executable Path on OS X and Linux
-(when (memq window-system '(mac ns x))
+(use-package exec-path-from-shell
+  :if (memq window-system '(mac ns))
+  :ensure t
+  :config
   (exec-path-from-shell-initialize))
 
 ;; evil mode
@@ -85,52 +51,76 @@
   :config
   (evil-collection-init))
 
-;; helm configuration
-(global-set-key (kbd "M-x") 'helm-M-x)
-(global-set-key (kbd "C-x f") 'helm-find-files)
-(global-set-key (kbd "C-x b") 'helm-buffers-list)
-(add-hook 'helm-after-initialize-hook
-          (lambda()
-            (define-key helm-map (kbd "TAB") #'helm-execute-persistent-action)
-            (define-key helm-map (kbd "<tab>") #'helm-execute-persistent-action)
-            (define-key helm-map (kbd "C-z") #'helm-select-action)
-            ))
-
-;; Golang
-(require 'go-mode)
-(require 'lsp-mode)
-
-(add-hook 'go-mode-hook #'lsp-deferred)
-
-;; Set up before-save hooks to format buffer and add/delete imports.
-;; Make sure you don't have other gofmt/goimports hooks enabled.
-(defun lsp-go-install-save-hooks ()
-  (add-hook 'before-save-hook #'lsp-format-buffer t t)
-  (add-hook 'before-save-hook #'lsp-organize-imports t t))
-(add-hook 'go-mode-hook #'lsp-go-install-save-hooks)
-
-(add-hook 'go-mode-hook
-          (lambda ()
-            (setq tab-width 4)
-	    (setq indent-tabs-mode 1)
-	    (show-paren-mode)
-            ))
-
-;; C mode
-(add-hook 'c-mode-hook #'lsp-deferred)
-
-;; lsp keybinding
-(add-hook 'lsp-mode-hook (lambda ()
-			   (local-set-key (kbd "M-<right>") #'lsp-find-definition)
-			   ))
-
-;; lsp configure
-(add-hook 'lsp-mode-hook (lambda ()
-			   (setq lsp-enable-file-watchers nil)
-			   ))
+(use-package helm
+  :ensure t
+  :bind
+  ("M-x" . helm-M-x)
+  ("C-x f" . helm-find-files)
+  ("C-x b" . helm-buffers-list)
+  ("C-x r" . helm-recentf)
+  ("C-x s" . helm-do-ag-project-root)
+  :config
+  (progn
+    (define-key helm-map (kbd "TAB") #'helm-execute-persistent-action)
+    (define-key helm-map (kbd "<tab>") #'helm-execute-persistent-action)
+    (define-key helm-map (kbd "C-z") #'helm-select-action)))
 
 ;; git
 (use-package magit
   :ensure t)
+  
+;; GO
 
-(load-theme 'doom-one-light t)
+(use-package lsp-mode
+  :ensure t
+  :commands (lsp lsp-deferred)
+  :bind (("M-<right>" . lsp-find-definition))
+  :config (progn
+	    (setq lsp-enable-file-watchers nil)
+	    (setq lsp-prefer-flymake nil)))
+
+(use-package lsp-ui
+  :ensure t
+  :commands lsp-ui-mode
+  :config (progn
+            ;; disable inline documentation
+            (setq lsp-ui-sideline-enable nil)
+            ;; disable showing docs on hover at the top of the window
+            (setq lsp-ui-doc-enable nil)))
+
+(use-package company
+  :ensure t
+  :config (progn
+            ;; don't add any dely before trying to complete thing being typed
+            ;; the call/response to gopls is asynchronous so this should have little
+            ;; to no affect on edit latency
+            (setq company-idle-delay 0)
+            ;; start completing after a single character instead of 3
+            (setq company-minimum-prefix-length 1)
+            ;; align fields in completions
+            (setq company-tooltip-align-annotations t)))
+
+(use-package flycheck
+  :ensure t)
+
+(defun go-mode-setting ()
+  (setq tab-width 4)
+  (setq indent-tabs-mode 1))
+
+(use-package go-mode
+  :ensure t
+  :bind (("C-x d" . dap-mode))
+  :config
+  :hook ((go-mode . lsp-deferred)
+	 (go-mode . show-paren-mode)
+	 (go-mode . go-mode-setting)
+	 (before-save . lsp-format-buffer)
+	 (before-save . lsp-organize-imports)))
+
+(use-package dap-mode
+  :defer t
+  :ensure t
+  :bind (("C-x h" . dap-hydra))
+  :config
+  (require 'dap-go)
+  (require 'dap-hydra))
